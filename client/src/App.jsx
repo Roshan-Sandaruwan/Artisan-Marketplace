efd8ed8f7072d3910287aea069b31ca4f7ffa2c5
Roshan Sandaruwan <112076077+Roshan-Sandaruwan@users.noreply.github.com>
2024-12-14 23:29:46 +0800

    update components file name

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,8 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
-import Header from "./componnets/header";
-import Footer from "./componnets/Footer";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
