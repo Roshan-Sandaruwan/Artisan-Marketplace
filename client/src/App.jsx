5075e584140147f23060945d360a76935db3e630
Roshan Sandaruwan <112076077+Roshan-Sandaruwan@users.noreply.github.com>
2024-12-14 13:47:46 +0800

    front files setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,17 +3,22 @@
 // import viteLogo from '/vite.svg'
 // import './App.css'
 
-function App() {
-  // const [count, setCount] = useState(0)
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
+import HomePage from "./pages/HomePage";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
+
+const App = () => {
   return (
-    <>
+    <Router>
+      <Routes>
+        <Route path="/register" element={<RegisterPage />}></Route>
+        <Route path="/login" element={<LoginPage />}></Route>
+        <Route path="/" element={<HomePage />}></Route>
+      </Routes>
+    </Router>
+  );
+};
 
-      <h1> Artify </h1>
-
-
-    </>
-  )
-}
-
-export default App
+export default App;
