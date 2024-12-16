fe2614893d4d795b6df9b9e8ec0a9cd62bfce3bb
Roshan Sandaruwan <112076077+Roshan-Sandaruwan@users.noreply.github.com>
2024-12-16 11:16:23 +0530

    product create test

--- server/server.js
+++ server/server.js
@@ -1,11 +1,32 @@
 import express from "express";
+import { connectDB } from "./config/db.js";
+import Product from "./models/Product.js";
+
 
 const app = express();
 
-app.get("/",(req, res)=>{
-  res.send("Server is ready");
+app.post("/products", async (req, res)=>{
+  const product = req.body; //user will send this data
+
+  if(!product.name || !product.price || !product.description || !product.image){
+    return res.status(400).json({ success:false, message: "Please Provide all fields" });
+  }
+
+  const newProduct = new Product(product);
+
+  try {
+    await newProduct.save();
+    res.status(201).json({success:true, data: newProduct});
+  } catch (error) {
+    console.error("Enter in create product", error.message);
+    res.status(500).json({success:false, message:"Server Error" });
+    
+  }
 });
 
+console.log(process.env.MONGO_URI);
+
 app.listen(5000,()=>{
+  connectDB();
   console.log("Server started at http://localhost:5000/");
 });
