36f0c68e04d2db726abcdbd66efc56ad226aa580
Roshan Sandaruwan <112076077+Roshan-Sandaruwan@users.noreply.github.com>
2024-12-18 00:35:22 +0800

    PORT Update

--- server/server.js
+++ server/server.js
@@ -1,19 +1,19 @@
 import express from "express";
 import { connectDB } from "./config/db.js";
-
 import productRoutes from "./routes/product.route.js";
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 //allow access JSON data in to body
-app.use(express.json()); 
+app.use(express.json());
 
-// main Product api
+//Product api
 app.use("/api/products", productRoutes);
 
-console.log(process.env.MONGO_URI);
+// console.log(process.env.MONGO_URI);
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   connectDB();
-  console.log("Server started at http://localhost:5000/");
+  console.log("Server started at http://localhost" + PORT);
 });
