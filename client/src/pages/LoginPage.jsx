e80338c3b7fc3e3b61b0f6218798c955dff3b6bb
osandaDev <osanda1208@gmail.com>
2024-12-21 18:21:03 +0530

    Updated

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -25,7 +25,7 @@ const LoginPage = () => {
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <h2 className="mt-10 text-center text-2xl font-bold tracking-tight text-gray-900">
-            Log in to your account
+            Log in to Your Account
           </h2>
         </div>
 
